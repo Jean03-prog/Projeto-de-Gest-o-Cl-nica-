bc984d91c8f660ac6a3ad2695a7d5a1ecf6f561f
taisavaleria <253159527+taisavaleria@users.noreply.github.com>
2026-03-30 16:13:07 -0300

    feat: implementado cores da Tela Inicial

---
+++ telas/tela_principal.py
@@ -0,0 +1,22 @@
+import customtkinter as ctk
+
+"""| Cor | Código | Uso |"""
+
+COR_ROXO = "#7c3aed" #Sidebar, botões principais, ícones
+COR_AZUL = "#3b82f6" #Cards, destaques, status
+COR_BRANCO ="#ffffff" #Fundo dos cards, superfícies
+COR_CINZA_CLARO = "#f9fafb" #Fundo da página
+COR_CINZA_ESCURO = "#111827" #Textos e títulos
+
+"""Cores dos Status na Tabela:
+Status | Cor de Fundo | Cor do Texto |"""
+
+FUNDO_REALIZADO = "#dcfce7" #verde claro fundo
+TEXTO_REALIZADO = "#166534" #verde escuro texto
+
+FUNDO_ACOMPANHAMENTO = "#dbeafe" #azul claro fundo
+TEXTO_ACOMPANHAMENTO = "#1e40af" #azul escuro texto
+class Dashbord(ctk.Frame):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+    
